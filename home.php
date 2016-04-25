ec741cbd1bbe207147582351a08dec6a51e23e43
Ben Hill <hillbenhill@gmail.com>
2016-04-25 15:56:36 +0100

    latest

--- home.php
+++ home.php
@@ -4,7 +4,8 @@
 /*Ben Hill
 Assigment : WE4.0 PHP Web App Assigment, Digital Skills Academy
   Student Id = D11128483
-  Date : 20016/04/22 */
+  Date : 20016/04/22
+ref http://www.bootply.com/103783  */
   session_start();
 
   if(!isset($_SESSION['user_session']))
@@ -47,104 +48,132 @@ Assigment : WE4.0 PHP Web App Assigment, Digital Skills Academy
           src="Images/haven_logo.png">
         </a>          
       </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="">Landing Page</a></li>
-            <li><a href="">About</a></li>
-            <li><a href="login.php">login</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
+      <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="">Landing Page</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="login.php">login</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
 
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-               <span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $row['user_name']; ?>&nbsp;<span class="caret"></span></a>
-               <ul class="dropdown-menu">
-                <li><a href="#"><span class="glyphicon glyphicon-user"></span>&nbsp;View Profile</a></li>
-                <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
-    
-    
-    <div class="body-container">
-
-      <div class="container">
-        <div class='alert alert-success'>
-          <button class='close' data-dismiss='alert'>&times;</button>
-          <strong>Hello '<?php echo $row['user_name']; ?></strong>  Welcome to the members page.
-        </div>
-      </div>
-
-      <div class="container">
-
-
-
-        <table class="table">
-          <tr>
-            <td><iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FCodingCage&amp;width&amp;height=290&amp;colorscheme=light&amp;show_faces=true&amp;header=true&amp;stream=false&amp;show_border=true&amp;appId=198210627014732" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:290px;" allowTransparency="true"></iframe>
-              <div style="height: 20px;">&nbsp;</div></td>
-
-              <td><a class="twitter-follow-button"
-                href="https://twitter.com/PradeepKhodke"
-                data-show-count="true"  data-size="large"  data-lang="en">
-                Follow @PradeepKhodke
-              </a>
-
-              <a class="twitter-follow-button"
-              href="https://twitter.com/CodingCage"
-              data-show-count="true"  data-size="large"  data-lang="en">
-              Follow @CodingCage
-            </a>
-
-          </td>
-
-          <td>
-            <div class="g-person" data-width="299" data-href="https://plus.google.com/103895933493952839746" data-layout="landscape" data-rel="publisher"></div>
-          </tr>
-        </table>
-
-
-        
-
-
-        <script type="text/javascript">
-        window.twttr = (function (d, s, id) {
-          var t, js, fjs = d.getElementsByTagName(s)[0];
-          if (d.getElementById(id)) return;
-          js = d.createElement(s); js.id = id;
-          js.src= "https://platform.twitter.com/widgets.js";
-          fjs.parentNode.insertBefore(js, fjs);
-          return window.twttr || (t = { _e: [], ready: function (f) { t._e.push(f) } });
-        }(document, "script", "twitter-wjs"));
-        </script>
-
-        <!-- Place this tag where you want the widget to render. -->
-
-
-        <!-- Place this tag after the last widget tag. -->
-        <script type="text/javascript">
-        (function() {
-          var po = document.createElement('script');
-          po.type = 'text/javascript';
-          po.async = true;
-          po.src = 'https://apis.google.com/js/platform.js';
-          var s = document.getElementsByTagName('script')[0];
-          s.parentNode.insertBefore(po, s);
-        })();
-        </script>
-      </div>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+             <span class="glyphicon glyphicon-user"></span>&nbsp;Hi' <?php echo $row['user_name']; ?>&nbsp;<span class="caret"></span></a>
+             <ul class="dropdown-menu">
+              <li><a href="#"><span class="glyphicon glyphicon-user"></span>&nbsp;View Profile</a></li>
+              <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Sign Out</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div><!--/.nav-collapse -->
     </div>
+  </nav>
+  <div class="bg"></div>
+<div class="jumbotron">
+  <h1>Haven</h1>
+  <p class="lead">Smart Homecare For Your loved Ones</p>
+</div>
 
+<div class="container">
+  <div class="row">
+    <h2>Page Content</h2>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+    Aliquam in felis sit amet augue.
+    <br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+    Aliquam in felis sit amet augue.
+    <br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+    Aliquam in felis sit amet augue.
+    <br>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+    Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+    dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+    Aliquam in felis sit amet augue.
   </div>
+ 
 </div>
+  
+  
+   <div class="container">
+      <!-- Example row of columns -->
+      <div class="row">
+        
+        <div class="col-md-4">
+          <h2 class = "navbar-brand">Haven </h2>
+          <p><div class='alert alert-success'>
+            <button class='close' data-dismiss='alert'>&times;</button>
+            <strong>Hello '<?php echo $row['user_name']; ?></strong>  Welcome to the members page.
+          </div></p>
+       </div>
+	   
+        <div class="col-md-4">
+          <h2>Haven Product Video</h2>
+          <p> <iframe src="images/HAVEN Facebook Profile page_Sanja_Trinki.jpg"></iframe> 
+          <div style="height: 20px;">&nbsp;</div></p>
+        </div>
+		<div class="col-md-4">
+          <h2>Follow</h2>
+          <p>
+          <a class="twitter-follow-button"
+            href="https://twitter.com/bennyOohill"
+            data-show-count="true"  data-size="large"  data-lang="en">
+            Follow @BennyOohill
+          </a>
+</div>
+      </div>
+	  
+
+<hr>
+
+      
+ 
+ </div><!-- /container-->
+    <script type="text/javascript">
+    window.twttr = (function (d, s, id) {
+      var t, js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src= "https://platform.twitter.com/widgets.js";
+      fjs.parentNode.insertBefore(js, fjs);
+      return window.twttr || (t = { _e: [], ready: function (f) { t._e.push(f) } });
+    }(document, "script", "twitter-wjs"));
+    </script>
+
+    <!-- Place this tag where you want the widget to render. -->
+
+    <!-- Place this tag after the last widget tag. -->
+    <script type="text/javascript">
+    (function() {
+      var po = document.createElement('script');
+      po.type = 'text/javascript';
+      po.async = true;
+      po.src = 'https://apis.google.com/js/platform.js';
+      var s = document.getElementsByTagName('script')[0];
+      s.parentNode.insertBefore(po, s);
+    })();
+    </script>
+<!-- paralax js to shrink jumbo with image scroll-->	
+	
+  <script type="text/javascript">
+var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});</script>
 
 
-</div>
-
-</div>
-<script src="bootstrap/js/bootstrap.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  
 </body>
 </html>
